3b177981a6c3f0f583eec20c691fbd0c2258a0bb
AAndreazza <alanaandreazza26@hotmail.com>
2024-04-03 16:07:36 -0300

    iniciando site com header

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    plugins:[
+        'gatsby-plugin-image',
+        'gatsby-plugin-sharp',
+        'gatsby-plugin-postcss',
+        'gatsby-transformer-sharp',
+    ]
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from '../components/pagina-inicial/Header'
+
+const Index = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+        </div>
+    )
+}
+
+export default Index
