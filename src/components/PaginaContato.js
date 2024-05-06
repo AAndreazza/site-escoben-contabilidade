b7f65f48e953c5ea1a1e9fba8218404977c78b2a
AAndreazza <alanaandreazza26@hotmail.com>
2024-05-06 15:33:16 -0300

    testando next do formsubmit

--- src/components/PaginaContato.js
+++ src/components/PaginaContato.js
@@ -42,7 +42,7 @@ const PaginaContato = () => {
                         <input type='email' name='email' id='email' autocomplete='email' placeholder='E-mail' className='mt-2.5 block w-full rounded-md border-0 px-3.5 py-2 sm:py-3 sm:mt-4  text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 bg-gray-100 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' />
                         <input type='tel' name='telefone' id='telefone' autocomplete='tel' placeholder='Celular com DDD' className='mt-2.5 block w-full rounded-md border-0 px-3.5 py-2 sm:py-3 sm:mt-4  text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 bg-gray-100 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' />
                         <textarea name='mensagem' id='mensagem' rows='4' placeholder='Mensagem' className='mt-2.5 block w-full rounded-md border-0 px-3.5 py-2 sm:py-3 sm:mt-4  text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 bg-gray-100 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6' />
-                        <input type='hidden' name='_next' value='http://localhost:8000/especialidades/' />
+                        <input type='hidden' name='_next' value='https://escobencontabilidade.netlify.app/' />
                         <input type='hidden' name='_captcha' value='false' />
 
                         <div className='mt-10 sm:mt-10 mb-10 text-center'>
